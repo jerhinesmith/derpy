be9ebe873f1d03aaf12149e3a45f3cb898fc1db9
Justin Rhinesmith <jerhinesmith@gmail.com>
2015-07-09 16:31:10 -0700

    Turn the attachment into json

--- models/message_attachment.rb
+++ models/message_attachment.rb
@@ -21,4 +21,28 @@ class MessageAttachment
 
     self
   end
+
+  def to_json
+    payload.to_json
+  end
+
+  private
+  def payload
+    @payload = { }
+
+    @payload.merge!(fallback:     fallback)     unless fallback.nil?
+    @payload.merge!(color:        color)        unless color.nil?
+    @payload.merge!(pretext:      pretext)      unless pretext.nil?
+    @payload.merge!(author_name:  author_name)  unless author_name.nil?
+    @payload.merge!(author_link:  author_link)  unless author_link.nil?
+    @payload.merge!(author_icon:  author_icon)  unless author_icon.nil?
+    @payload.merge!(title:        title)        unless title.nil?
+    @payload.merge!(title_link:   title_link)   unless title_link.nil?
+    @payload.merge!(text:         text)         unless text.nil?
+    @payload.merge!(fields:       fields)       unless fields.empty?
+    @payload.merge!(image_url:    image_url)    unless image_url.nil?
+    @payload.merge!(thumb_url:    thumb_url)    unless thumb_url.nil?
+
+    @payload
+  end
 end
