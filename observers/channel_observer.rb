eb277e82acc3d0136a9c378582c877900fb07121
Justin Rhinesmith <jerhinesmith@gmail.com>
2015-08-14 12:47:43 -0700

    Oops

--- observers/channel_observer.rb
+++ observers/channel_observer.rb
@@ -6,13 +6,13 @@ class ChannelObserver
   end
 
   def self.call(channel, incoming_message)
-    # Protect against responding to bots
-    return true if (!respond_to_bots? && incoming_message.posted_by_bot?)
-
     new(channel, incoming_message).call
   end
 
   def call
+    # Protect against responding to bots
+    return true if (!respond_to_bots? && incoming_message.posted_by_bot?)
+
     raise NotImplementedError, "#call must be implemented by subclasses"
   end
 
