c6d0631025da4f3cfc181a4467484f0833f5c10c
Aaron Macy <aaronmacy@gmail.com>
2015-09-04 12:55:01 -0700

    Fix Kc

--- services/kc.rb
+++ services/kc.rb
@@ -1,20 +1,30 @@
-require 'twitter'
+require "twitter"
 
 class Kc
 
-  ACCOUNT_NAME = 'kc_advice'
-  NUMBER_OF_TWEERS = 200
+  ACCOUNT_NAME = "kc_advice"
+  NUMBER_OF_TWEETS = 200
+  CONSUMER_KEY = "pqAHeAczxGlq1m0K7V4WRnOgj"
+  CONSUMER_SECRET = ENV["TWITTER_CONSUMER_SECRET"]
+  ACCESS_TOKEN = "23369679-mAKj448qj40u3in93Ldw6HkooiOKspxnUwRNsloA7"
+  ACCESS_SECRET = ENV["TWITTER_ACCESS_SECRET"]
+
+  attr_reader :tweets
 
   def initialize
-    @tweets = Twitter.user_timeline(ACCOUNT_NAME, NUMBER_OF_TWEETS)
+    @tweets = _twitter_client.user_timeline(ACCOUNT_NAME, :count => NUMBER_OF_TWEETS)
   end
 
   def random_tweet
-    tweets.sample['text']
+    @tweets.sample.text
   end
 
-  private
-
-    attr_accessor :tweets
-
+  def _twitter_client
+    @twitter_client ||= Twitter::REST::Client.new do |config|
+      config.consumer_key        = CONSUMER_KEY
+      config.consumer_secret     = CONSUMER_SECRET
+      config.access_token        = ACCESS_TOKEN
+      config.access_token_secret = ACCESS_SECRET
+    end
+  end
 end
