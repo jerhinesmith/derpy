d07e5f2d1e04cfd1dd2416b87e2df7e805a2138a
Matthew Werner <m@mjw.io>
2015-11-04 13:26:43 -0800

    add multiple symbol support

--- observers/finance.rb
+++ observers/finance.rb
@@ -2,20 +2,20 @@ require_relative 'channel_observer'
 
 class Finance < ChannelObserver
   def call
-    match = incoming_message.text.match(/\$([A-Z]{1,4})/)
-    if match
-      symbol = match[1].to_s.upcase
-
+    symbols = incoming_message.text.scan(/\$([A-Z]{1,4})/).flatten
+    if symbols.any?
+      symbol = symbols.shift
       message = OutgoingMessage.new(
         channel:  "##{incoming_message.channel_name}",
         username: 'greedcjh',
         icon_url: 'http://imgur.com/MeYf2Ee.jpg'
       )
 
+      symbol_keys = symbols.map{|sym| "$#{sym}" }
       message.attachments << MessageAttachment.new(
-        title:      "$#{symbol}",
+        title:      "$#{symbol} vs #{symbol_keys.join(', ')}",
         title_link: "https://www.google.com/finance?q=#{symbol}",
-        image_url:  "http://chart.finance.yahoo.com/z?s=#{symbol}&t=7d&q=l&l=on&z=s&p=m50,m200"
+        image_url:  "http://chart.finance.yahoo.com/z?s=#{symbol}&t=7d&q=l&l=on&z=s&p=m50,m200,v&c=#{symbols.join(',')}"
       )
 
       channel.post(message)
