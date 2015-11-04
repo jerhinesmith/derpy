2aecee2127676223a313dd472fb7a421a0d1c561
Matthew Werner <m@mjw.io>
2015-11-04 12:18:23 -0800

    add trigger word attr

--- models/incoming_message.rb
+++ models/incoming_message.rb
@@ -1,7 +1,7 @@
 class IncomingMessage
   attr_accessor :token, :team_id, :team_domain, :service_id, :channel_id,
                 :channel_name, :timestamp, :user_id, :user_name, :text,
-                :bot_id, :bot_name
+                :bot_id, :bot_name, :trigger_word
 
   def initialize(attributes = {})
     attributes.each do |k, v|
