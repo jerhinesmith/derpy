d7274628fb629622c4e60652c961373d9246de99
Justin Rhinesmith <jerhinesmith@gmail.com>
2015-11-07 16:43:35 -0800

    Be more explicit about start and end

--- observers/markov.rb
+++ observers/markov.rb
@@ -4,14 +4,17 @@ class Markov < ChannelObserver
   TUPLE_LENGTH = 3
 
   def call
-    input = "#{incoming_message.text} __MARKOV_END__"
+    input = incoming_message.text
 
     words = input.split(" ")
 
-    if words.length >= TUPLE_LENGTH
-      (0...(words.length - TUPLE_LENGTH)).each do |i|
-        Gram.add(*words[i..(i + TUPLE_LENGTH)])
-      end
+    return unless words.length > TUPLE_LENGTH
+
+    words.unshift(*%w(__MARKOV_START_A__ __MARKOV_START_B__ __MARKOV_START_C__))
+    words.push '__MARKOV_END__'
+
+    (0...(words.length - TUPLE_LENGTH)).each do |i|
+      Gram.add(*words[i..(i + TUPLE_LENGTH)])
     end
   end
 end
