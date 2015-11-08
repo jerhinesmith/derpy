9538817026d06018a9542c4baaa45b11d28aeb14
Justin Rhinesmith <jerhinesmith@gmail.com>
2015-11-07 17:42:07 -0800

    Forgot to make these instance vars

--- services/porker_cjh.rb
+++ services/porker_cjh.rb
@@ -2,7 +2,7 @@ class PorkerCjh
   attr_reader :sword1, :sword2, :sword3
 
   def initialize
-    sword1, sword2, sword3 = "__MARKOV_START_A__", "__MARKOV_START_B__", "__MARKOV_START_C__"
+    @sword1, @sword2, @sword3 = "__MARKOV_START_A__", "__MARKOV_START_B__", "__MARKOV_START_C__"
   end
 
   def call
